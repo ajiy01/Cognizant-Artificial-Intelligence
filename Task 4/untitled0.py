55a47e92f37d98c7485415f604877c60540715ac
Ajay Iyer <92404808+ajiy01@users.noreply.github.com>
2022-12-22 15:13:16 -0700

    Add files via upload

---
+++ Task 4/untitled0.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 19 15:08:39 2022
+
+@author: ajayiyer
+"""
+
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+
+df = pd.read_csv('/Users/ajayiyer/Desktop/Data Science Practice Projects/Cognizant Artificial Intelligence/Task 3/sales.csv')
+df.drop(columns=["Unnamed: 0"], inplace=True, errors='ignore')
+
+# Create target variable and predictor variables
+def create_target_and_predictors(
+    data: pd.DataFrame = None, 
+    target: str = "estimated_stock_pct"
+):
+    """
+    This function takes in a Pandas DataFrame and splits the columns
+    into a target column and a set of predictor variables, i.e. X & y.
+    These two splits of the data will be used to train a supervised 
+    machine learning model.
+
+    :param      data: pd.DataFrame, dataframe containing data for the 
+                      model
+    :param      target: str (optional), target variable that you want to predict
+
+    :return     X: pd.DataFrame
+                y: pd.Series
+    """
+
+    # Check to see if the target variable is present in the data
+    if target not in data.columns:
+        raise Exception(f"Target: {target} is not present in the data")
+    
+    X = data.drop(columns=[target])
+    y = data[target]
+    return X, y
+
+# Train algorithm
+def train_algorithm_with_cross_validation(
+    X: pd.DataFrame = None, 
+    y: pd.Series = None
+):
+    """
+    This function takes the predictor and target variables and
+    trains a Random Forest Regressor model across K folds. Using
+    cross-validation, performance metrics will be output for each
+    fold during training.
+
+    :param      X: pd.DataFrame, predictor variables
+    :param      y: pd.Series, target variable
+
+    :return
+    """
+
+    # Create a list that will store the accuracies of each fold
+    accuracy = []
+
+    # Enter a loop to run K folds of cross-validation
+    for fold in range(0, K):
+
+        # Instantiate algorithm and scaler
+        lr = LinearRegression()
+        scaler = StandardScaler()
+
+        # Create training and test samples
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test=0.2, random_state=42)
+
+        # Scale X data, we scale the data because it helps the algorithm to converge
+        # and helps the algorithm to not be greedy with large values
+        scaler.fit(X_train)
+        X_train = scaler.transform(X_train)
+        X_test = scaler.transform(X_test)
+
+        # Train model
+        trained_model = lr.fit(X_train, y_train)
+
+        # Generate predictions on test sample
+        y_pred = trained_model.predict(X_test)
+
+        # Compute accuracy, using mean absolute error
+        mae = mean_absolute_error(y_true=y_test, y_pred=y_pred)
+        accuracy.append(mae)
+        print(f"Fold {fold + 1}: MAE = {mae:.3f}")
+
+    # Finish by computing the average MAE across all folds
+    print(f"Average MAE: {(sum(accuracy) / len(accuracy)):.2f}")
